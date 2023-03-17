b704c1889b9af95cae10bdec2436ce54e33c3800
Peter Perhac <PeterPerhac@users.noreply.github.com>
2023-03-17 10:49:01 +0000

    Slackify tool converted to Rust

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+
+fn slackify(c: char) -> String {
+    match c {
+        letter if letter.is_alphabetic() => format!(":alphabet-white-{}:", letter),
+        number if number.is_whitespace() => ":blank:".to_string(),
+        other => other.to_string(),
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let relevant_args = &args[1..];
+    let mut iter = relevant_args.iter().peekable();
+    while let Some(arg) = iter.next() {
+        arg.chars().for_each(|c| print!("{}", slackify(c)));
+        if iter.peek().is_some() { print!(":blank:"); }
+    }
+    println!();
+}
